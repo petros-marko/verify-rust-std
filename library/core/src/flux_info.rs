ff1157ca4f5ddcf34876260052c9cdadf2d32f49
Ranjit Jhala <rjhala@ucsd.edu>
2025-07-15 12:21:43 -0700

    DONE: pat

--- library/core/src/flux_info.rs
+++ library/core/src/flux_info.rs
@@ -4,12 +4,21 @@
 /// See the following link for more information on how extensible properties for primitive operations work:
 /// <https://flux-rs.github.io/flux/guide/specifications.html#extensible-properties-for-primitive-ops>
 #[flux::defs {
+
+    fn char_to_int(c:char) -> int { 
+        cast(c) 
+    } 
+
     property ShiftRightByFour[>>](x, y) {
         16 * [>>](x, 4) == x
     }
 
-    property MaskBy15[&](x, y) {
-        [&](x, y) <= y
+    property MaskLess[&](x, y) {
+        [&](x, y) <= x && [&](x, y) <= y
+    }
+
+    property ShiftLeft[<<](n, k) {
+        0 < k => n <= [<<](n, k)
     }
 }]
 const _: () = {};
